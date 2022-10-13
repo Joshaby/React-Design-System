122d08b5cf149bde1c273088e9e950e2beb66604
Joshaby <jose.azevedo@academico.ifpb.edu.br>
2022-10-13 00:52:26 -0300

    Remove pasta stories e inicia implementação dos componentes

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ export function App() {
 
   return (
     <>
-      <h1 className="font-bold text-5xl text-violet-500">Hello world</h1>
-      <button className='bg-violet-500 font-medium m-4 px-4 py-2 rounded text-white hover:bg-violet-600'>
+      <h1 className="font-bold text-5xl text-cyan-300">Hello world</h1>
+      <button className='bg-cyan-500 font-medium m-4 px-4 py-2 rounded text-white hover:bg-violet-600'>
         Enviar
       </button>
     </>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,23 @@
 module.exports = {
   content: ['./src/**/*.tsx'],
   theme: {
+    fontSize: {
+      xs: 14,
+      s: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      '2xl': 32
+    },
+    colors: {
+      'gray-900': '#121214',
+      'gray-800': '#202024',
+      'gray-400': '#7C7C8A',
+      'gray-200': '#C4C4CC',
+      'gray-100': '#E1E1EA',
+      'cyan-500': '#81D8F7',
+      'cyan-300': '#9BE1FB'
+    },
     extend: {
       fontFamily: {
         sans: 'Inter, sans-serif'
